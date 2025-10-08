7adb6b18e5e2b3ff79c094060d18a91f9fe1ed4e
Davong <david.laptop@dewaweb.com>
2025-10-08 21:20:21 +0700

    Prettify logging for progress update endpoint

--- src/app/api/v1/jobs/[job_id]/progress/route.ts
+++ src/app/api/v1/jobs/[job_id]/progress/route.ts
@@ -1,7 +1,18 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
-export async function PATCH(request: Request, props: { params: Promise<{ job_id: string }> }) {
+export async function PATCH(
+  request: Request,
+  props: { params: Promise<{ job_id: string }> },
+) {
   const params = await props.params;
   // TODO: Implement job progress update
-  return NextResponse.json({ message: `Job ${params.job_id} progress updated` });
+
+  const body = await request.json();
+  console.log(
+    `Received the following params and body\nparams: ${JSON.stringify(params, null, 2)}\nbody: ${JSON.stringify(body, null, 2)}`,
+  );
+
+  return NextResponse.json({
+    message: `Job ${params.job_id} progress updated`,
+  });
 }
